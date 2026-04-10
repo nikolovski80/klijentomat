1f1ebe59664b402b24cc11c3c9221e221d59a11e
nikolovski80 <nikolovski80@gmail.com>
2026-04-11 01:51:04 +0200

    Fix: CORS allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,8 +11,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.APP_URL, "http://localhost:3000"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
